24184d7293cb9d2dda3c56dd10e42c63e5023e1a
Noyo1811 <84664564+Noyo1811@users.noreply.github.com>
2021-12-10 11:42:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function setup(){
+ canvas =   createCanvas(400,400);
+canvas.center();
+}
+function takeSnapshot(){
+    save("MyFilterPng.png")
+}
+function preload(){
+
+}
+
+function draw(){
+
+}
